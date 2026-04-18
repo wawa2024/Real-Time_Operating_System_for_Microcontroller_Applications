cf83dcc1d2055081a923fd883c077d8ebf547bc7
lamiika <47070356+lamiika@users.noreply.github.com>
2026-04-18 19:06:18 +0300

    Added crude zoom functions

--- software/src/appCore/graph_task.cpp
+++ software/src/appCore/graph_task.cpp
@@ -8,6 +8,7 @@
 #include <SPI.h>
 #include <esp32-oscilloscope.h>
 #include <driver/adc.h>
+#include <driver/timer.h>
 #include <hmiCore.h>
 
 /////////////////////////////// 2.Macros ///////////////////////////////
@@ -18,9 +19,18 @@
 #define GRID_OFFSET_Y (RESOLUTION_Y / GRID_COUNT_Y)
 #define REFRESH_RATE_MS 300
 #define ADC_RESOLUTION 4096
-#define BUF_LEN 2560
+#define BUF_LEN 10240
 
 /////////////////////////////// 3.Types ////////////////////////////////
+
+enum class State {
+  TRIGGER,
+  TRIGGER_SETTINGS,
+  SCALE,
+  POSITION,
+  MEASURE
+};
+
 //////////////////////////// 4.Declarations ////////////////////////////
 //////////////////////////// 4.1.Variables /////////////////////////////
 //////////////////////////// 4.2.Functions /////////////////////////////
@@ -30,11 +40,13 @@ static QueueHandle_t q = NULL;
 
 //////////////////////////// 5.1.Variables /////////////////////////////
 
-static uint16_t trigger_level = 3584;
+static uint16_t trigger_level = 512;
 uint16_t samples[BUF_LEN];
 size_t write_head = 0;
 size_t read_head = 0;
-uint16_t zoom_level = 1;
+uint16_t horizontal_zoom = 10;
+uint16_t vertical_zoom = 10;
+State state = State::TRIGGER;
 
 //////////////////////////// 5.2.Functions /////////////////////////////
 
@@ -52,7 +64,7 @@ void adc_task(void *pvParameters) {
 	while (true) {
 		uint16_t ch1 = adc1_get_raw(ADC1_CHANNEL_5);
 		add_sample(ch1);
-		vTaskDelay(pdMS_TO_TICKS(10));
+		vTaskDelay(pdMS_TO_TICKS(1));
   }
 }
 
@@ -83,26 +95,83 @@ void trigger() {
 }
 
 void draw_trigger() {
-	tft.drawFastHLine(0, RESOLUTION_Y - trigger_level / 17, RESOLUTION_X, TFT_CYAN);
+	tft.drawFastHLine(0, RESOLUTION_Y - trigger_level / 17 * vertical_zoom / 10, RESOLUTION_X, TFT_CYAN);
 }
 
 void draw_graph() {
-	uint8_t previous_value = RESOLUTION_Y - samples[read_head] / 17;
+	uint8_t previous_value = RESOLUTION_Y - samples[read_head] / (18 - vertical_zoom);
 	for (size_t i = 1; i < RESOLUTION_X; i++) {
-		size_t index = (read_head + i) % BUF_LEN;
-		uint8_t value = RESOLUTION_Y - samples[index] / 17;
+		size_t index = (read_head + i) % BUF_LEN * horizontal_zoom / 10;
+		uint8_t value = RESOLUTION_Y - samples[index] / 17 * vertical_zoom / 10;
 		tft.drawLine(i-1, previous_value, i, value, TFT_GREEN);
 		previous_value = value;
 	}
 }
 
 void draw_grid() {
-	for (uint16_t i = GRID_OFFSET_X; i < RESOLUTION_Y; i += GRID_OFFSET_X) {
+	for (uint16_t i = GRID_OFFSET_X; i < RESOLUTION_X; i += GRID_OFFSET_X) {
 		tft.drawFastVLine(i, 0, RESOLUTION_Y, TFT_DARKGREY);
 	}
-	for (uint16_t i = GRID_OFFSET_Y; i < RESOLUTION_X; i += GRID_OFFSET_Y) {
+	for (uint16_t i = GRID_OFFSET_Y; i < RESOLUTION_Y; i += GRID_OFFSET_Y) {
 		tft.drawFastHLine(0, i, RESOLUTION_X, TFT_DARKGREY);
 	}
+}
+
+void button_logic() {
+  hmiEventData_t data = getinputs(q);
+  uint32_t& inputs = data.inputs;
+
+  if( inputs & BTN_ESC ) oscilloscope_deinit();
+
+  if ( inputs & BTN_SCALE ) {
+    if (state == State::SCALE) {
+      state = State::POSITION;
+    } else {
+      state = State::SCALE;
+    }
+  } else if ( inputs & BTN_TRIGGER ) {
+    state = State::TRIGGER;
+  } else if ( inputs & BTN_MEASURE ) {
+    state = State::MEASURE;
+  }
+
+  switch (state) {
+    case State::TRIGGER:
+      if (inputs & BTN_UP) {
+        trigger_level += 128;
+        if (trigger_level > ADC_RESOLUTION)
+          trigger_level = ADC_RESOLUTION;
+      } else if (inputs & BTN_DOWN) {
+        if (trigger_level < 128)
+          trigger_level = 0;
+        else
+          trigger_level -= 128;
+      } else if (inputs & BTN_SCALE) {
+          state = State::SCALE;
+      }
+      break;
+
+    case State::SCALE:
+      if (inputs & BTN_UP) {
+        vertical_zoom++;
+      } else if (inputs & BTN_DOWN) {
+        if (vertical_zoom != 0) {
+          vertical_zoom--;
+        }
+      } else if (inputs & BTN_RIGHT) {
+        horizontal_zoom++;
+      } else if (inputs & BTN_LEFT) {
+        if (horizontal_zoom != 0) {
+          horizontal_zoom--;
+        }
+      }
+      break;
+
+    default:
+        break;
+  }
+
+  Serial.println((int)state);
 }
 
 void ui_task(void *pvParameters) {
@@ -121,32 +190,13 @@ void ui_task(void *pvParameters) {
     if(mutex_take()) {
 
       while(true) {
-
-        hmiEventData_t data = getinputs(q);
-        uint32_t& inputs = data.inputs;
-
-        if( inputs & BTN_ESC ) oscilloscope_deinit();
-
-        if ( inputs & BTN_UP ) {
-          trigger_level += 500;
-          if (trigger_level > ADC_RESOLUTION) {
-            trigger_level = ADC_RESOLUTION;
-          }
-        } else if ( inputs & BTN_DOWN ) {
-          if (trigger_level < 500) {
-            trigger_level = 0;
-          } else {
-            trigger_level -= 500;
-          }
-        }
-
+        button_logic();
         tft.fillScreen(TFT_BLACK);
         trigger();
         draw_grid();
         draw_graph();
         draw_trigger();
         vTaskDelay(pdMS_TO_TICKS(100));
-        Serial.println(samples[write_head]);
       }
 
     }
